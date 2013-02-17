266cb7f68a91715ac14f0b9e3f33840e11bd4785
Harrison Tan <harrisontan@harrison-tans-macbook-pro.local>
2013-02-17 13:00:00 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bnpomrails::Application.config.secret_token = '02b190e57c513852b5da75b10d339276c5963fbea5dee2b3de45530551fd04f88255bd9a3c0e74a06aac1793d89047257cdfe1bbc951d883570f805d8009ff0e'
