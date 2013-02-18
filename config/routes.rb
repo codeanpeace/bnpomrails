5591d4d697b28078f76e6c3f90be7fcc890ffa18
Harrison Tan <harrisontan@harrison-tans-macbook-pro.local>
2013-02-18 00:48:20 -0500

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bnpomrails::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
